d1ca3efc9d2f38344aa938a959d6682ecc4a68e1
Bobo100 <33471758+Bobo100@users.noreply.github.com>
2023-03-23 10:22:01 +0800

    新增donwload image

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import InputImage from "./component/InputImage";
 import "./css/Home.scss";
+import image from "../image/dmitry-demidko-eBWzFKahEaU-unsplash.jpg"
 
 
 export const Home = () => {
@@ -10,8 +11,22 @@ export const Home = () => {
         // navigate('/result', { state: { image: value } });
 
     };
+
+    function downloadImage() {
+        // Create a new anchor element
+        const link = document.createElement('a');
+        // Set the href attribute of the anchor element to the image URL
+        link.href = image;
+        // Set the download attribute of the anchor element to the file name you want to save
+        link.download = 'dmitry-demidko-eBWzFKahEaU-unsplash.jpg';
+        // Trigger a click event on the anchor element
+        // This will automatically download the image
+        link.click();
+    }
+
     return (
         <div className="home">
+            <button onClick={downloadImage} >Download</button>
             <InputImage labelId="image" placeholderText="請選擇圖片" onChange={handleFileChange} />
         </div>
     );
